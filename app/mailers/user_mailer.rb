490d9862d335dcf52c43f23b0f39524d3213ced6
joshcrist20 <joshcrist20@gmail.com>
2015-08-10 17:15:29 -0600

    Update user_mailer.rb
    Changed from email to "joshcrist20@gmail.com"

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-    default from: "Harry's <welcome@harrys.com>"
+    default from: "Josh Crist <joshcrist20@gmail.com>"
 
     def signup_email(user)
         @user = user
