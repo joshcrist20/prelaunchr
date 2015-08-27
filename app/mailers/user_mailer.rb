cea880b70824581696b1c39c82fb7c5aaa7cafa0
joshcrist20 <joshcrist20@gmail.com>
2015-08-27 13:25:44 -0600

    Update to email address and message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-    default from: "Josh Crist <joshcrist20@gmail.com>"
+    default from: "Josh Crist <jcrist@zultimate.co>"
 
     def signup_email(user)
         @user = user
-        @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+        @twitter_message = "#DojoDay2015 is coming. Excited to be @ZFOCO on September 26th! ."
 
         mail(:to => user.email, :subject => "Thanks for signing up!")
     end
